cc0ce1878358f35da5b5799aa26e2c72dc55d6dc
antonio <antonio@antonios-MacBook-Pro.local>
2023-12-25 15:34:53 +0100

    feat: add server side

---
+++ server/js/unconf-tx.js
@@ -0,0 +1,18 @@
+const fs = require('node:fs').promises;
+const CSV_FILE = `csv/main.eth.csv`;
+const JSON_FILE = `json/unconfirmed-txs.json`;
+
+const main = async () => {
+	const data = await fs.readFile(CSV_FILE, 'utf8').catch((e) => (`${e}`));
+	const rows = data.split(`\n`).slice(1, -1);
+	const unconfirmedTxs = rows.map(onRows).slice(-50);
+	const r = await fs.writeFile(JSON_FILE, JSON.stringify(unconfirmedTxs), 'utf8').catch((e) => (`${e}`));
+	console.log(`message: ${r}`);
+};
+
+const onRows = (e) => {
+	const values = e.split(",");
+	return {time: values[3]?.slice(0,16), count: values[8]}
+}
+
+main();
