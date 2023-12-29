c1c225200d7b57c048c6d5cec29093cb171b80ad
antonio <antonio@antonios-MacBook-Pro.local>
2023-12-29 18:12:38 +0100

    feat: add gas price chart

--- server/js/unconf-tx.js
+++ server/js/unconf-tx.js
@@ -4,6 +4,7 @@ const JSON_FILE = `json/unconfirmed-txs.json`;
 
 const main = async () => {
 	const data = await fs.readFile(CSV_FILE, 'utf8').catch((e) => (`${e}`));
+	const columns = data.split(`\n`)[0];
 	const rows = data.split(`\n`).slice(1, -1);
 	const unconfirmedTxs = rows.map(onRows).slice(-200);
 	const r = await fs.writeFile(JSON_FILE, JSON.stringify(unconfirmedTxs), 'utf8').catch((e) => (`${e}`));
@@ -11,7 +12,7 @@ const main = async () => {
 
 const onRows = (e) => {
 	const values = e.split(",");
-	return {time: values[3]?.slice(0,16), count: values[8]}
+	return {time: values[3]?.slice(0,16), count: values[8], high_gas_price: values[9], low_gas_price: values[11]}
 }
 
 main();
