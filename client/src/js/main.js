8d815fb126249e68c74b14bf45d011d188898480
antonio <antonio@antonios-MacBook-Pro.local>
2023-12-30 14:34:28 +0100

    add fee chart

--- client/src/js/main.js
+++ client/src/js/main.js
@@ -1,7 +1,9 @@
 const { createLineChart } = require('./line.js');
 const { createGasPriceChart } = require('./gas-price.js');
+const { createFeePriceChart } = require('./fee.js');
 const data = require('../../../server/json/unconfirmed-txs.json');
 
 createLineChart(data);
 createGasPriceChart(data);
+createFeePriceChart(data);
 
--- server/js/unconf-tx.js
+++ server/js/unconf-tx.js
@@ -4,7 +4,6 @@ const JSON_FILE = `json/unconfirmed-txs.json`;
 
 const main = async () => {
 	const data = await fs.readFile(CSV_FILE, 'utf8').catch((e) => (`${e}`));
-	const columns = data.split(`\n`)[0];
 	const rows = data.split(`\n`).slice(1, -1);
 	const unconfirmedTxs = rows.map(onRows).slice(-200);
 	const r = await fs.writeFile(JSON_FILE, JSON.stringify(unconfirmedTxs), 'utf8').catch((e) => (`${e}`));
@@ -12,7 +11,16 @@ const main = async () => {
 
 const onRows = (e) => {
 	const values = e.split(",");
-	return {time: values[3]?.slice(0,16), count: values[8], high_gas_price: values[9], low_gas_price: values[11]}
+	return {
+		time: values[3]?.slice(0,16), 
+		count: values[8], 
+		high_gas_price: values[9], 
+		low_gas_price: values[11],
+		high_priority_fee: values[12],
+		// medium_priority_fee,
+		low_priority_fee: values[14],
+		// base_fee
+	}
 }
 
 main();
